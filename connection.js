e3b9bf35909287e4b018522c8c920a7431fdee7a
kenny svp <kenny@stonevalleypartners.com>
2017-12-17 22:21:58 -0700

    connect to mysql

---
+++ connection.js
@@ -0,0 +1,9 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Abc123!!",
+    port: "3306",
+    //database: "test"
+});
+module.exports = connection;
--- populate-db.js
+++ populate-db.js
@@ -1,4 +1,9 @@
 var http = require("./http-service.js");
+var db = require("./connection.js");
+var path = require("path");
+var data = [];
+var flattenedData = [];
+
 
 http
   .get()
@@ -8,3 +13,25 @@ http
   .catch(error => {
     console.log(error.message);
   });
+
+  function connectToSql() {
+    db.connect(function (err) {
+        if (err)
+            throw err
+        else {
+            console.log("Connected to MySQL");
+            // db.query("CREATE DATABASE IF NOT EXISTS test", function (err, result) {
+            //     if (err) throw err;
+            //     console.log(result);
+            // });
+            // var sql = "CREATE TABLE IF NOT EXISTS `test` (symbol VARCHAR(65), price DECIMAL(65), size DECIMAL(65), time DECIMAL(65))";
+            // db.query(sql, function (err, result) {
+            //     if (err) throw err;
+            //     console.log("table created");
+            //     addToDb();
+            // });
+        }
+    });
+};
+
+connectToSql();

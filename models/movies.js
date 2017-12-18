592a4325e5a1dbc1fa9b42721dd58c0d481bc641
kenny svp <kenny@stonevalleypartners.com>
2017-12-18 15:03:19 -0700

    update readme and add an npm start script

--- models/movies.js
+++ models/movies.js
@@ -1,19 +1,10 @@
 var db = require('../config/connection.js'); //reference of dbconnection.js  
 var Price = {  
-    getAllPrices: function(callback) {  
-        return db.query("Select * from test", callback);  
+    getAllMovies: function(callback) {  
+        return db.query("Select * from movies", callback);  
     },  
-    getPriceById: function(time, callback) {  
-        return db.query("select * from test where time=?", [id], callback);  
-    },  
-    addPrice: function(Price, callback) {  
-        return db.query("Insert into test values(?,?,?)", [Price.symbol, Price.price, Price.size, Price.time], callback);  
-    },  
-    deletePrice: function(time, callback) {  
-        return db.query("delete from test where time=?", [time], callback);  
-    },  
-    updatePrice: function(time, Price, callback) {  
-        return db.query("update test set symbol=?,price=?,status=? where time=?", [Price.symbol, Price.price, Price.size, time], callback);  
-    }  
+    getMovieById: function(time, callback) {  
+        return db.query("select * from movies where id=?", [id], callback);  
+    }    
 };  
 module.exports = Price;

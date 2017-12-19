2584e33031fc93427573a3de619e57340d98557c
kenny svp <kenny@stonevalleypartners.com>
2017-12-19 09:21:06 -0700

    still trying to reslove connection issues

--- models/movies.js
+++ models/movies.js
@@ -1,22 +1,10 @@
 var db = require('../config/connection.js'); //reference of dbconnection.js  
 var Price = {
   getAllMovies: function (callback) {
-    db.connect(function (err) {
-      if (err)
-        throw err
-      else {
-       return db.query("Select * from movies", callback);
-      }
-    })
+    return db.query("Select * from movies", callback);
   },
-  getMovieById: function (id, callback) {
-    db.connect(function (err) {
-      if (err)
-        throw err
-      else {
-        db.query("select * from movies where id=?", [id], callback);
-      }
-    })
+  getMovieById: function (time, callback) {
+    return db.query("select * from movies where id=?", [id], callback);
   }
 };
 module.exports = Price;

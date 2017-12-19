ca3de59246c881509e1f12d57650a221cdcf573e
kenny svp <kenny@stonevalleypartners.com>
2017-12-19 09:57:10 -0700

    still trying to reslove connection issues

--- models/gather.js
+++ models/gather.js
@@ -29,7 +29,7 @@ var Gather = {
           else console.log(result);
         });
         var sql = "CREATE TABLE if not exists `movies` (`id` int(11) NOT NULL,`adult` varchar(10) DEFAULT NULL,`backdrop_path` varchar(100) DEFAULT NULL, `genre_ids` text, `original_language` varchar(20) DEFAULT NULL,`original_title` varchar(100) DEFAULT NULL,`overview` text,`popularity` decimal(10,6) DEFAULT NULL,`poster_path` varchar(100) DEFAULT NULL, `release_date` datetime DEFAULT NULL, `title` varchar(100) DEFAULT NULL, `video` varchar(10) DEFAULT NULL, `vote_average` decimal(4,2) DEFAULT NULL, `vote_count` int(11) DEFAULT NULL, PRIMARY KEY (`id`))";
-        function addToDb(time, callback) {
+        var addToDb = function(time, callback) {
           console.log("adding to db");
           for (var i = 0; i < data.length; i++) {
             flattenedData.push([data[i].id, data[i].adult, data[i].backdrop_path, data[i].genre_ids, data[i].original_language, data[i].original_title, data[i].overview, data[i].popularity, data[i].poster_path, data[i].release_date, data[i].title, data[i].video, data[i].vote_average, data[i].vote_count]);
@@ -43,13 +43,13 @@ var Gather = {
               console.log('data inserted' + result);
             }
           });
-        }
+        };
         db.query(sql, function (err, result) {
           if (err) {
             throw err;
           }
           else {
-            return console.log("table created" + result);
+            console.log("table created" + result);
             addToDb();
           }
         });

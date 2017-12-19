10740b8ef6237e04b3432bee00c446562e55d8df
kenny svp <kenny@stonevalleypartners.com>
2017-12-19 09:04:01 -0700

    add connection.end()

--- models/gather.js
+++ models/gather.js
@@ -21,13 +21,14 @@ var Gather = {
             db.query("CREATE DATABASE IF NOT EXISTS candidate_leek", function (err, result) {
               if (err) throw err;
               console.log(result);
+              connection.end();
             });
             var sql = "CREATE TABLE if not exists `movies` (`id` int(11) NOT NULL,`adult` varchar(10) DEFAULT NULL,`backdrop_path` varchar(100) DEFAULT NULL, `genre_ids` text, `original_language` varchar(20) DEFAULT NULL,`original_title` varchar(100) DEFAULT NULL,`overview` text,`popularity` decimal(10,6) DEFAULT NULL,`poster_path` varchar(100) DEFAULT NULL, `release_date` datetime DEFAULT NULL, `title` varchar(100) DEFAULT NULL, `video` varchar(10) DEFAULT NULL, `vote_average` decimal(4,2) DEFAULT NULL, `vote_count` int(11) DEFAULT NULL, PRIMARY KEY (`id`))";
             db.query(sql, function (err, result) {
               if (err) throw err;
               else {
                 console.log("table created" + result);
-
+                connection.end();
               }
             });
           }
@@ -39,19 +40,25 @@ var Gather = {
 
   },
   addToDb: function (time, callback) {
-    console.log("adding to db");
-    for (var i = 0; i < data.length; i++) {
-      flattenedData.push([data[i].id, data[i].adult, data[i].backdrop_path, data[i].genre_ids, data[i].original_language, data[i].original_title, data[i].overview, data[i].popularity, data[i].poster_path, data[i].release_date, data[i].title, data[i].video, data[i].vote_average, data[i].vote_count]);
-      //console.log(flattenedData);
-    }
-    return db.query("INSERT INTO movies (id, adult, backdrop_path, genre_ids,  original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count ) values ?", [flattenedData], function (err, result) {
-      if (err) {
-        throw err;
-      }
+    db.connect(function (err) {
+      if (err)
+        throw err
       else {
-        console.log(result);
-        return result;
-      }
+        console.log("adding to db");
+        for (var i = 0; i < data.length; i++) {
+          flattenedData.push([data[i].id, data[i].adult, data[i].backdrop_path, data[i].genre_ids, data[i].original_language, data[i].original_title, data[i].overview, data[i].popularity, data[i].poster_path, data[i].release_date, data[i].title, data[i].video, data[i].vote_average, data[i].vote_count]);
+          //console.log(flattenedData);
+        }
+        return db.query("INSERT INTO movies (id, adult, backdrop_path, genre_ids,  original_language, original_title, overview, popularity, poster_path, release_date, title, video, vote_average, vote_count ) values ?", [flattenedData], function (err, result) {
+          if (err) {
+            throw err;
+          }
+          else {
+            console.log(result);
+            connection.end();
+          }
+        });
+      };
     });
   },
 };
--- models/movies.js
+++ models/movies.js
@@ -1,10 +1,22 @@
 var db = require('../config/connection.js'); //reference of dbconnection.js  
 var Price = {
   getAllMovies: function (callback) {
-    return db.query("Select * from movies", callback);
+    return db.connect(function (err) {
+      if (err)
+        throw err
+      else {
+        db.query("Select * from movies", callback);
+      }
+    })
   },
-  getMovieById: function (time, callback) {
-    return db.query("select * from movies where id=?", [id], callback);
+  getMovieById: function (id, callback) {
+    return db.connect(function (err) {
+      if (err)
+        throw err
+      else {
+        db.query("select * from movies where id=?", [id], callback);
+      }
+    })
   }
 };
 module.exports = Price;

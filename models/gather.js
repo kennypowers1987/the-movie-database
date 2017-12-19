87fdc439fde02830a862891b37a633953004d675
kenny svp <kenny@stonevalleypartners.com>
2017-12-19 09:07:17 -0700

    still trying to reslove connection issues

--- models/gather.js
+++ models/gather.js
@@ -21,14 +21,14 @@ var Gather = {
             db.query("CREATE DATABASE IF NOT EXISTS candidate_leek", function (err, result) {
               if (err) throw err;
               console.log(result);
-              connection.end();
+              db.end();
             });
             var sql = "CREATE TABLE if not exists `movies` (`id` int(11) NOT NULL,`adult` varchar(10) DEFAULT NULL,`backdrop_path` varchar(100) DEFAULT NULL, `genre_ids` text, `original_language` varchar(20) DEFAULT NULL,`original_title` varchar(100) DEFAULT NULL,`overview` text,`popularity` decimal(10,6) DEFAULT NULL,`poster_path` varchar(100) DEFAULT NULL, `release_date` datetime DEFAULT NULL, `title` varchar(100) DEFAULT NULL, `video` varchar(10) DEFAULT NULL, `vote_average` decimal(4,2) DEFAULT NULL, `vote_count` int(11) DEFAULT NULL, PRIMARY KEY (`id`))";
             db.query(sql, function (err, result) {
               if (err) throw err;
               else {
                 console.log("table created" + result);
-                db.connection.end();
+                db.end();
               }
             });
           }
